94d5011a0fd6d638f25bdaa83ecb9f2fdd48cb01
Duarte <durtemendonca1234@gmail.com>
2024-10-21 17:20:14 +0100

    Bingo ball improvement
    Added code to display different ball sprites depending on the number. Added rigidbody to bingo ball

--- bingo/scripts/bingo_ball.gd
+++ bingo/scripts/bingo_ball.gd
@@ -1,20 +1,33 @@
 extends Node2D
 
-@onready var letter_text: Label = $Sprite2D/LetterText
-@onready var number_text: Label = $Sprite2D/NumberText
+@export  var ball_sprite: Sprite2D
+@export var letter_text: Label
+@export  var number_text: Label 
 
+var ball_sprite_dict = {
+	"B": preload("res://textures/BingoBalls/BBall.png"),
+	"I": preload("res://textures/BingoBalls/IBall.png"),
+	"N": preload("res://textures/BingoBalls/NBall.png"),
+	"G": preload("res://textures/BingoBalls/GBall.png"),
+	"O": preload("res://textures/BingoBalls/OBall.png")
+}
+	
 func setText(number)->void:
-	print("number",number)
-	number_text.text = str(number)
+	
+	var letter: String
 	
 	if number >= 1 and number <= 15:
-		letter_text.text = "B"
+		letter = "B"
 	elif number >= 16 and number <= 30:
-		letter_text.text = "I"
+		letter = "I"
 	elif number >= 31 and number <= 45:
-		letter_text.text = "N"
+		letter = "N"
 	elif number >= 46 and number <= 60:
-		letter_text.text = "G"
+		letter = "G"
 	elif number >= 61 and number <= 75:
-		letter_text.text = "O"
-		
+		letter = "O"
+	
+	print("Letter",letter)
+	letter_text.text = letter
+	number_text.text = str(number)
+	ball_sprite.texture = ball_sprite_dict[letter]
