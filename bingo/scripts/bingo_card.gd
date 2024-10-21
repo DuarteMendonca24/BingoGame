4629df6020bad36b9176823b80fa9d26f658a565
Duarte <durtemendonca1234@gmail.com>
2024-10-21 13:27:52 +0100

    Textures
    Imported textures for bingo card, bingo ball and ball tube

--- bingo/scripts/bingo_card.gd
+++ bingo/scripts/bingo_card.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var rng = RandomNumberGenerator.new()
-@onready var bingo_grid: GridContainer = $BingoGrid
+@onready var bingo_grid: GridContainer = $Sprite2D/BingoGrid
 
 var  column_dict = {
 	"B": [],
